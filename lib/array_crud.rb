433b16087f2adc603e3bca19bdbebee50e8f1d3a
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-19 00:08:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,8 +18,7 @@ def remove_element_from_end_of_array(array)
   array.pop
 end
 
-def remove_element_from_start_of_array
-  array = ["A", "B", "C"]
+def remove_element_from_start_of_array(array)
   array.shift 
 end
 
