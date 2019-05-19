7f8f084e41e8bd5f02ed4e893a7eacb735eb9388
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-19 00:07:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,9 +10,8 @@ def add_element_to_end_of_array(array, element)
    array << element 
 end
 
-def add_element_to_start_of_array
-  array = ["Evan"]
-  array.unshift ("Eric")
+def add_element_to_start_of_array(array, element)
+  array.unshift (element)
 end
 
 def remove_element_from_end_of_array
