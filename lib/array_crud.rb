5f822fa49bf5dc7883d81b1bab2b20c124310f10
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-19 00:10:26 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,37 +3,33 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Evan", "Anderson", "Eric", "Scott"]
+  array = ["Evan", "Anderson", "Eric", "Scott"]
 end
 
-def add_element_to_end_of_array
-  ["Evan"]
-  add_element_to_end_of_array << "Anderson"
+def add_element_to_end_of_array(array, element)
+   array << element 
 end
 
-def add_element_to_start_of_array
-  ["Evan"]
-  add_element_to_start_of_array.unshift ("Eric")
+def add_element_to_start_of_array(array, element)
+  array.unshift (element) 
 end
 
-def remove_element_from_end_of_array["A", "B", "C"]
-  remove_element_from_end_of_array.pop
-  
+def remove_element_from_end_of_array(array)
+  array.pop
 end
 
-def remove_element_from_start_of_array["A", "B", "C"]
-  remove_element_from_start_of_array.shift 
-  
+def remove_element_from_start_of_array(array)
+  array.shift 
 end
 
-def retrieve_element_from_index["array", "index_number"]
-  retrieve_element_from_index[1]
+def retrieve_element_from_index(array, index_number)
+    array [index_number]
 end
 
-def retrieve_first_element_from_array["A", "B", "C"]
-  retrieve_first_element_from_array[0]
+def retrieve_first_element_from_array(array)
+  array[0]
 end
 
-def retrieve_last_element_from_array["A", "B", "C"]
-  retrieve_last_element_from_array[-1]
+def retrieve_last_element_from_array(array)
+  array[-1]
 end
