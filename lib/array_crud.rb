13c48f006ba3987a7f4e2134a9ceb62a52f95969
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-19 00:08:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ def add_element_to_start_of_array(array, element)
   array.unshift (element) 
 end
 
-def remove_element_from_end_of_array
+def remove_element_from_end_of_array(array)
   array =["A", "B", "C"]
   array.pop
 end
